5d2bda057afd450a5b87537acbec692472dd7256
A'zamjon Abdumuxtorov <azamjonabdumuxtorov1220@gmail.com>
2024-01-04 19:49:42 +0500

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by A'zamjon Abdumuxtorov on 04/01/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
